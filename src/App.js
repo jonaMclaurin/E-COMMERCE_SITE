49f8abe1061557107f115d6f87b6ca3a3d9b80af
Jonatan <tuskerfieldmac@Jony-MacBook-Pro.local>
2020-07-22 04:41:10 -0500

    update with a show page, collection preview componentand collection item

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
-
-
-
-const HatsPage = () => {
- return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  )
-}
-
-
+import ShowPage from './pages/shop/shop.component'
 
 
 function App() {
@@ -21,7 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route  path='/shop/hats' component={HatsPage} />
+        <Route  path='/shop' component={ShowPage} />
       </Switch>
     </div>
   );
