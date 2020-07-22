30f29ca5c35236681ca3c635356bc1680810f1db
Jonatan <tuskerfieldmac@Jony-MacBook-Pro.local>
2020-07-22 06:01:36 -0500

    signin

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './sign-in-and-sign-up.component.scss'
+import SigIn from '../../components/sing-in/sign-in.component'
+
+
+const SignInAndSignUpPage = () => {
+    return(
+        <div className='sign-in-and-sign-up'>
+        <SigIn/>
+        </div>
+    )
+}
+
+export default SignInAndSignUpPage
