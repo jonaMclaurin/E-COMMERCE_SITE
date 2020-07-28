ad35070319ce2d58bc670a384107c2d29229cf4c
Jonatan <tuskerfieldmac@Jony-MacBook-Pro.local>
2020-07-28 01:39:37 -0500

    moved directory into its own reducer, created respected selectors and updated components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './redux/store'
 
 
 
@@ -12,7 +13,9 @@ import store from './redux/store'
 ReactDOM.render(
  <Provider store={ store }>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor} >
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector }from 'reselect'
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
-import CartItem from '../../components/cart-item/cart-item.component';
+
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
 
 
